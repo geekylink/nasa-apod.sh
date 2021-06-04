c4b43d2b0fe3f1aa8e487ecaa1ea93e651721f06
James <geekylink@gekinzuku.com>
2021-06-04 13:52:22 -0400

    NASA - A Picture a Day Downloader

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nasa-apod
 NASA A Picture a Day Download Shell Script (Makes cool wallpapers)
+
+Modify bgDir in nasa-apod.sh to be a directory on your machine.
+
+Cool use for this project:
+
+Add this to cron, then make your wallpaper auto-change every X minutes, awesome rotating space wallpaper for Linuxs.
---
+++ nasa-apod.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+bgDir="/media/Pictures/backgrounds/"
+dateStr=$(date "+%Y%m%d")
+
+download_pic () {
+
+    wget -O astropix.html https://apod.nasa.gov/apod/astropix.html
+    fileName=$(cat astropix.html | grep -i img | sed "s/^.*=\"//" | sed "s/\".*$//")
+    fileExt=$(cat astropix.html | grep -i img | sed "s/^.*\.//" | sed "s/\".*$//")
+    fullURL="https://apod.nasa.gov/apod/$fileName"
+    saveFile="nasa-$dateStr.$fileExt"
+    fullFile="$bgDir$saveFile"
+    echo "Saving to $fullFile"
+    wget -O $fullFile $fullURL 
+    rm astropix.html
+
+}
+
+is_file_exist () {
+
+    saveFile="nasa-$dateStr.*"
+    fullFile="$bgDir$saveFile"
+
+    fileExt=$(echo $fullFile | sed "s/^.*\.//")
+
+    saveFile="nasa-$dateStr.$fileExt"
+    fullFile="$bgDir$saveFile"
+
+
+    if [[ ! -f $fullFile ]] ; then
+        return 1
+    fi
+
+    return 0
+
+}
+
+is_file_exist
+
+if [[ $? == 1 ]] ; then
+    echo "Downloading"
+    download_pic
+else
+    echo "Nothing to do"
+fi
